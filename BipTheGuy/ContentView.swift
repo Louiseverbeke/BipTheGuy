cd3d88941d9d7d2f9218f8cc011ad132422f1f59
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-12 13:24:07 -0400

    after 3.3 biptheguyfinal

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -30,12 +33,20 @@ struct ContentView: View {
                
             Spacer()
             
-            Button {
-               //TODO: Button action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo library", systemImage: "photo.fill.on.rectangle.fill")
+                            
             }
-            
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let selectedImage = try? await
+                            selectedPhoto?.loadTransferable(type: Image.self) else {
+                        print("😡ERROR: Could not get Image from loadTransferrable")
+                        return
+                    }
+                    bipImage = selectedImage
+                }
+            }
         }
         .padding()
     }
